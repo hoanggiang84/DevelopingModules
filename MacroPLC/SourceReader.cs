f8a94eacd110c3e01cd6137ed239edeb3b6c0652
hoanggiang84 <hoanggiang24484@yahoo.com>
2017-06-30 11:50:48 +0700

    Finish SourceReader

--- MacroPLC/SourceReader.cs
+++ MacroPLC/SourceReader.cs
@@ -1,4 +1,5 @@
-﻿using UtilitiesVS2008WinCE;
+﻿using System;
+using UtilitiesVS2008WinCE;
 
 namespace MacroPLC
 {
@@ -7,14 +8,51 @@ namespace MacroPLC
         private string source;
         public SourceReader(string source)
         {
-            this.source = source;
+            CurrentIndex = 0;
+            Source = source;
         }
+
+        public string Source
+        {
+            get { return source; } 
+            set
+            {
+                if (value.IsNullOrWhite())
+                    throw new InvalidSourceCodeException("Source is null or only white space");
+                source = value;
+            }
+        }
+
+        public int CurrentIndex { get; private set; }
 
         public string ReadNextLine()
         {
-            if (source == string.Empty)
-                return string.Empty;
-            return null;
+            if (CurrentIndex >= source.Length)
+                return null;
+
+            var lineContent = string.Empty;
+            while (CurrentIndex < source.Length)
+            {
+                var currentChar = source[CurrentIndex++];
+                if(currentChar == '\n')
+                    break;
+                lineContent += currentChar;
+            }
+
+            return lineContent.IsNullOrWhite() ? string.Empty : lineContent;
+        }
+
+        public void ResetIndex()
+        {
+            CurrentIndex = 0;
+        }
+    }
+
+    public class InvalidSourceCodeException : Exception
+    {
+        public InvalidSourceCodeException(string msg) :
+            base(msg)
+        {
         }
     }
 }
--- MacroPLCTest/SourceReaderTest.cs
+++ MacroPLCTest/SourceReaderTest.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-using System.Text;
-using MacroPLC;
+﻿using MacroPLC;
 using NUnit.Framework;
 
 namespace MacroPLCTest
@@ -11,21 +7,101 @@ namespace MacroPLCTest
     public class SourceReaderTest
     {
         [Test]
-        public void ReadNextLine_nullSource()
+        public void InvalidSourceTest()
         {
-            var reader = new SourceReader(null);
+            Assert.Throws<InvalidSourceCodeException>(() => new SourceReader(null),"null source");
+            Assert.Throws<InvalidSourceCodeException>(() => new SourceReader(string.Empty), "empty source");
+            Assert.Throws<InvalidSourceCodeException>(() => new SourceReader(" \t\r \n"), "white source");
+        }
+
+        [Test]
+        public void ReadNextLine_OneLine()
+        {
+            var source = "dbete";
+            var reader = new SourceReader(source);
             var lineContent = reader.ReadNextLine();
+            Assert.AreEqual(source, lineContent);
+        }
+
+        [Test]
+        public void ReadNextLine_OneLineWithEnter()
+        {
+            var source = "dbete\n";
+            var reader = new SourceReader(source);
+            var lineContent = reader.ReadNextLine();
+            Assert.AreEqual(source.Substring(0,source.Length - 1), lineContent);
+        }
+
+        [Test]
+        public void ReadBeyondSource_OneLineWithEnter()
+        {
+            var source = "dbete\n";
+            var reader = new SourceReader(source);
+            var lineContent = reader.ReadNextLine();
+            Assert.AreEqual(source.Substring(0, source.Length - 1), lineContent);
+            lineContent = reader.ReadNextLine();
+            Assert.IsNull(lineContent);
+            lineContent = reader.ReadNextLine();
             Assert.IsNull(lineContent);
         }
 
         [Test]
-        public void ReadNextLine_EmptySource()
+        public void ResetSourceIndex()
         {
-            var source = string.Empty;
+            var source = "dbete\n";
             var reader = new SourceReader(source);
             var lineContent = reader.ReadNextLine();
-            Assert.IsEmpty(lineContent);
+            Assert.AreEqual(source.Substring(0, source.Length - 1), lineContent);
+            reader.ResetIndex();
+            lineContent = reader.ReadNextLine();
+            Assert.AreEqual(source.Substring(0, source.Length - 1), lineContent);
         }
 
+        [Test]
+        public void ReadSource_ManyLinesWithEnter()
+        {
+            var source = "abc\n  \n efgh\n";
+            var reader = new SourceReader(source);
+            var lineContent = reader.ReadNextLine();
+            Assert.AreEqual("abc", lineContent,"first line");
+            lineContent = reader.ReadNextLine();
+            Assert.IsEmpty(lineContent,"second line");
+            lineContent = reader.ReadNextLine();
+            Assert.AreEqual(" efgh", lineContent,"third line");
+            lineContent = reader.ReadNextLine();
+            Assert.IsNull(lineContent,"end of source");
+        }
+
+        [Test]
+        public void ReadSource_withExpectedResults()
+        {
+            var whiteStr = " \t \r";
+            var sourceLines = new string[]
+                             {
+                                 "x1=2;",
+                                 whiteStr,
+                                 "dfefef sfege",
+                                 "// dfegeg",
+                                 whiteStr,
+                                 "if (true) then"                                 
+                             };
+
+            var source = string.Join("\n", sourceLines);
+            var reader = new SourceReader(source);
+            var lineContent = reader.ReadNextLine();
+            Assert.AreEqual("x1=2;", lineContent, "first line");
+            lineContent = reader.ReadNextLine();
+            Assert.IsEmpty(lineContent, "second line");
+            lineContent = reader.ReadNextLine();
+            Assert.AreEqual("dfefef sfege", lineContent, "third line");
+            lineContent = reader.ReadNextLine();
+            Assert.AreEqual("// dfegeg", lineContent, "fourth line");
+            lineContent = reader.ReadNextLine();
+            Assert.IsEmpty(lineContent, "fifth line");
+            lineContent = reader.ReadNextLine();
+            Assert.AreEqual("if (true) then", lineContent, "sixth line");
+            lineContent = reader.ReadNextLine();
+            Assert.IsNull(lineContent, "end of source");
+        }
     }
 }
