fda7b3437d2b0f98b1c0d31ac3e68b97373ac427
hoanggiang84 <hoanggiang24484@yahoo.com>
2017-06-30 10:07:43 +0700

    Config first test

--- MacroPLCTest/MacroUnitTest.cs
+++ MacroPLCTest/MacroUnitTest.cs
@@ -2,10 +2,18 @@
 using System.Linq;
 using System.Collections.Generic;
 using System.Text;
+using NUnit.Framework;
 
 namespace MacroPLCTest
 {
+    [TestFixture]
     public class MacroUnitTest
     {
+        [Test]
+        public void DetectNextLine()
+        {
+            
+        }
+
     }
 }
